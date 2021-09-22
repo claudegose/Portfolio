4766a8d5d8babf1602902c23b1ceaad85d57f55a
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-22 11:28:49 +0200

    Styling Integrated for TopBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Topbar from "./components/topbar/Topbar";
+
 
 function App() {
   return (
     <div className="app">
+      <Topbar />
+        .sections
 
 
     </div>
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./topbar.css";
+
+export default function Topbar() {
+    return(
+
+        <div className="topbar">
+            hey
+            <div className="sections">
+Hey
+
+            </div>
+        </div>
+    )
+
+}
+
