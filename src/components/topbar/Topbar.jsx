f6ff9ca016f1e9a512cbdb0b267768c225322fdc
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-22 11:39:26 +0200

    Import all components to App.js

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./topbar.css";
 
+
 export default function Topbar() {
     return(
 
         <div className="topbar">
-            hey
-            <div className="sections">
-Hey
-
-            </div>
+HEYYYYYY
         </div>
     )
 
