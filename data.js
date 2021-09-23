bf49d4068068cf506304f72a946f858694996299
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-23 11:36:16 +0200

    Styling for app.scss and data.js added to root

---
+++ data.js
@@ -0,0 +1,43 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Weather App",
+        img: "assets/weatherapp.png",
+    },
+
+    {
+        id: 2,
+        title: "Snake Game",
+        img: "assets/snakegame.png",
+    },
+
+    {
+        id: 3,
+        title: "Crud Operation",
+        img: "assets/crud.png",
+    },
+];
+
+export const javascriptPortfolio = [
+    {
+        id: 1,
+        title: "Snake Game",
+        img: "assets/snakegame.png",
+    },
+];
+
+export const reactjsPortfolio = [
+    {
+        id: 1,
+        title: "Weather App",
+        img: "assets/weatherapp.png",
+    },
+];
+
+export const phpPortfolio = [
+    {
+        id: 1,
+        title: "CRUD Operation",
+        img: "assets/crud.png",
+    },
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
-import Portfolio from "./components/portfolio/Portfolio";
-import Works from "./components/works/Works";
-import Testimonials from "./components/testimonials/Testimonials";
+import React from "react";
+import { useState } from "react";
+// here goes the components:
 import Contact from "./components/contact/Contact";
-import "./app.scss";
-
-
-
+import Intro from "./components/intro/Intro";
+import Menu from "./components/menu/Menu";
+import Portfolio from "./components/portfolio/Portfolio";
+import PortfolioList from "./components/portfolioList/PortfolioList";
+import Topbar from "./components/topbar/Topbar";
+import Works from "./components/works/Works";
+import "./app.css"
 
 function App() {
-  return (
-    <div className="app">
-      <Topbar />
-        <div className="sections">
 
-            <Intro />
-            <Portfolio />
-            <Works />
-            <Testimonials />
-            <Contact />
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    return (
+        <div className="app">
+            <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <div className="sections">
+                <Intro />
+                <Portfolio />
+                <Works />
+                <Contact />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
+
+export default App;
 
 export default App;
