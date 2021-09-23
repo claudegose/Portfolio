702cf58e48f1665b2b416f30e8e8b4e62480d014
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-23 11:40:59 +0200

    Menu bar completed

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./menu.css"
+
+
+export default function Menu({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"menu " + (menuOpen && "active")}>
+            <ul onClick={() => setMenuOpen(false)}>
+                <li>
+                    <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#works">Works</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
